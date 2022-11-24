5af79b613b828d098513d2a3a0ad9f4488108b56
bufaonanet <douglas.bufaonanet@outlook.com>
2022-11-24 09:28:49 -0300

    Add project files.

---
+++ Consumidor/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ConsumerConfig
+{
+    GroupId = "devio",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, desenvolvedor.io.Curso>(config)
+    .SetValueDeserializer(new AvroDeserializer<desenvolvedor.io.Curso>(schemaRegistry).AsSyncOverAsync())
+    .Build();
+
+consumer.Subscribe("cursos");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Mensagem: {result.Message.Value.Descricao}" );
+}
---
+++ Produtor/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, desenvolvedor.io.Curso>(config)
+    .SetValueSerializer(new AvroSerializer<desenvolvedor.io.Curso>(schemaRegistry))
+    .Build();
+
+var message = new Message<string, desenvolvedor.io.Curso>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = new desenvolvedor.io.Curso
+    {
+        Id = Guid.NewGuid().ToString(),
+        Descricao = "Curso de Apache Kafka"
+    }
+};
+
+var resul = await producer.ProduceAsync("cursos", message);
+
+Console.WriteLine($"{resul.Offset}");
